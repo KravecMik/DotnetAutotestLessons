a8305b995aab1726e4c97dd31c5847f8e88673fa
Mikhail Kravchenko <kravec.mik@ya.ru>
2025-10-16 23:43:45 +0700

    апыавппфвыап

---
+++ Lesson1/Привет.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class Привет
+    {
+    }
+}
